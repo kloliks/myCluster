8c8468e1402fe2ef3efaa0c798b66bc42001bcb4
klolik <klolik@gmail.com>
2016-12-23 06:20:18 +0300

    ...

--- app/src/main/java/com/example/yos/mycluster/Layer.java
+++ app/src/main/java/com/example/yos/mycluster/Layer.java
@@ -3,11 +3,38 @@ package com.example.yos.mycluster;
 import java.util.ArrayList;
 
 class Layer {
+    private int deepness;
     private Layer[] layers;
     private Cluster cluster;
     private ArrayList<Marker> markers;
+    private boolean clustering;
+
+    Layer(int deepness) {
+        this.deepness = deepness;
+        clustering = true;
+    }
+    Layer() {
+        this(1);
+    }
+
+
+    void disableClustering() {
+        clustering = false;
+    }
+    void enableClustering() {
+        clustering = true;
+        reCluster();
+    }
+    void reCluster() {
+        if (!clustering)
+            return;
+    }
 
     void addMarker(Marker marker) {
+        if (cluster != null) {
+
+        }
+
         if (markers == null) {
             markers = new ArrayList<Marker>();
         }
@@ -17,6 +44,8 @@ class Layer {
         } else {
             markers.add(marker);
         }
+
+        reCluster();
     }
 
     void deleteMarker(Marker marker) {}
--- app/src/main/java/com/example/yos/mycluster/Marker.java
+++ app/src/main/java/com/example/yos/mycluster/Marker.java
@@ -1,5 +1,7 @@
 package com.example.yos.mycluster;
 
+import android.graphics.Point;
+
 import com.google.android.gms.maps.model.LatLng;
 
 class Marker {
@@ -13,6 +15,19 @@ class Marker {
     Marker(String title, LatLng coordinates) {
         this.title = title;
         this.coordinates = coordinates;
+    }
+
+    private static double fromLatitude(double latitude) {
+        double radians = Math.toRadians(latitude + 90) / 2;
+        return Math.toDegrees(Math.log(Math.tan(radians)));
+    }
+    Point getTileCoordinates(int zoom) {
+        int dimension = 1 << zoom;
+        double longitudeSpan = 360.0 / dimension;
+        double mercator = fromLatitude(coordinates.latitude);
+        int x = (int) (coordinates.longitude / longitudeSpan) + 180;
+        int y = (int) (mercator / 360 * dimension) + 180;
+        return new Point(x, y);
     }
 
     @Override
